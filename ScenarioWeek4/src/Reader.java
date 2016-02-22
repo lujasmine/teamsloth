ae41f2c67e7122a3edb0f2c6b358c98db54c666c
JAMIE LAW <jamie.law.14@ucl.ac.uk>
2016-02-22 15:24:33 +0000

    Rename files
    Renamed files for clarity

--- ScenarioWeek4/src/Reader.java
+++ ScenarioWeek4/src/Reader.java
@@ -7,7 +7,7 @@ public static void readData() {
 	  int[] coordListSize = new int[numberOfQuestions]; //Number of points in each question
 	  int eachQuestion = 0; //Counter value for each question
 	  try{
-		for (String line : Files.readAllLines(Paths.get("guards.txt"))) {
+		for (String line : Files.readAllLines(Paths.get("Part1Questions.txt"))) {
 				for (String question : line.split("\\d:")) { //Question contains all the coordinates for each question
 					if(question.contains("{")){ //Get rid of null questions
 				        question = question.replaceAll("\\s+", ""); //Get rid of whitespace
@@ -21,13 +21,13 @@ public static void readData() {
 				        for (String pair : question.split("\\{|\\,|\\}")) { //Get each value
 				    		if(pair.matches("\\d|\\d\\.\\d+")==true){ //Get rid of null values and only accept numbers or decimals
 				    			double coord = Double.parseDouble(pair); //Convert the string coordinate into a double
-				    			System.out.println("Coord:" + coord);
+				    			//System.out.println("Coord:" + coord);
 				    		}
 				    	}
 				        eachQuestion++;
 				        coordList[eachQuestion] = question; //Put the currently read list into array
 				        coordListSize[eachQuestion] = questionSize; //Put the currently read list size into array
-				        System.out.println("Size for question " + eachQuestion + ":" + coordListSize[eachQuestion]);
+				        //System.out.println("Size for question " + eachQuestion + ":" + coordListSize[eachQuestion]);
 					}
 			    }	
 			}
